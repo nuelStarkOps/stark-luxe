a7099405ba6e4be218ed9313854d705953359352
Emmanuel Nwokeocha <nwokeochaemma01@gmail.com>
2025-10-28 17:21:58 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/stark-luxe/",
+  base: "/stark-luxe",
   server: {
     host: "::",
     port: 8080,
